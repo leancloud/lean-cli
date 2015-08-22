bd390d9ae8e9f5ca343bd7ced2ebdc4f19a0f1b0
asaka <aisk1988@gmail.com>
2015-08-23 00:15:46 +0800

    move to cli

--- lean/main.go
+++ lean/main.go
@@ -1,24 +1,27 @@
 package main
 
 import (
-	"fmt"
-	"github.com/spf13/cobra"
+	"github.com/codegangsta/cli"
+	"os"
 )
 
 func main() {
-	var rootCmd = &cobra.Command{
-		Use:   "lean",
-		Short: "LeanEngine command line tool",
+	app := cli.NewApp()
+
+	app.Name = "lean"
+	app.Usage = "make an explosive entrance"
+	app.Version = VERSION
+
+	app.Flags = []cli.Flag{}
+
+	app.Action = func(ctx *cli.Context) {
+		if len(ctx.Args()) == 0 {
+			cli.ShowAppHelp(ctx)
+			return
+		}
+		subCmd := ctx.Args()[0]
+		println(subCmd)
 	}
 
-	var versionCmd = &cobra.Command{
-		Use:   "version",
-		Short: "Print version info",
-		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println(VERSION)
-		},
-	}
-	rootCmd.AddCommand(versionCmd)
-
-	rootCmd.Execute()
+	app.Run(os.Args)
 }
