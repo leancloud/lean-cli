169f7dbc6c538ff045d1490cf836783a9fbdd5cd
asaka <aisk1988@gmail.com>
2016-08-02 15:03:18 +0800

    feat: use XDG config dir instead of `${HOME}/.leancloud`

--- lean/utils/home.go
+++ lean/utils/home.go
@@ -3,12 +3,12 @@ package utils
 import (
 	"os"
 	"os/user"
+	"path/filepath"
 )
 
 // HomeDir returns the system current user home directory
 func HomeDir() string {
 	homeDir := os.Getenv("HOME")
-
 	if homeDir != "" {
 		return homeDir
 	}
@@ -18,4 +18,14 @@ func HomeDir() string {
 		panic(err)
 	}
 	return currentUser.HomeDir
+}
+
+// ConfigDir returns the current user's XDG config dir
+func ConfigDir() string {
+	configDir := os.Getenv("XDG_CONFIG_HOME")
+	if configDir != "" {
+		return configDir
+	}
+
+	return filepath.Join(HomeDir(), ".config")
 }
