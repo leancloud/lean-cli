1120c33802c5d9bf697acd1a14b36f46daa1ec48
asaka <aisk1988@gmail.com>
2016-08-19 17:19:47 +0800

    feat: update dependences

--- lean/vendor/github.com/aisk/wizard/wizard.go
+++ lean/vendor/github.com/aisk/wizard/wizard.go
@@ -27,7 +27,7 @@ func Ask(questions []Question) {
 	for _, question := range questions {
 		printQuestion(question)
 		if question.Input != nil {
-			print(">> ")
+			print(" => ")
 			if question.Input.Hidden {
 				password, _ := speakeasy.Ask("(input will be hidden)")
 				*question.Input.Result = password
@@ -50,17 +50,17 @@ func printQuestion(qustion Question) {
 func printAnswers(question Question) {
 	blue := color.New(color.FgBlue).SprintFunc()
 	for i, answer := range question.Answers {
-		fmt.Printf(" %s) %s\n", blue(i), answer.Content)
+		fmt.Printf(" %s) %s\n", blue(i+1), answer.Content)
 	}
 }
 
 func scanAnswerNumber(question Question) func() {
 	for true {
-		fmt.Print(" >> ")
+		fmt.Print(" => ")
 		var input string
 		fmt.Scanln(&input)
 		for i, answer := range question.Answers {
-			if strconv.Itoa(i) == input {
+			if strconv.Itoa(i+1) == input {
 				return answer.Handler
 			}
 		}
