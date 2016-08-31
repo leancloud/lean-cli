09c867ea260cf85c1c7412fab3aa5698f5b53d97
asaka <aisk1988@gmail.com>
2016-08-31 17:35:15 +0800

    refactor: update deps

--- lean/vendor/golang.org/x/net/ipv4/sockopt_asmreq_posix.go
+++ lean/vendor/golang.org/x/net/ipv4/sockopt_asmreq_posix.go
@@ -2,29 +2,30 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// +build darwin dragonfly freebsd netbsd openbsd windows
+
 package ipv4
 
 import (
 	"net"
 	"os"
-	"syscall"
 	"unsafe"
 
 	"golang.org/x/net/internal/iana"
 )
 
-func setsockoptIPMreq(fd syscall.Handle, name int, ifi *net.Interface, grp net.IP) error {
+func setsockoptIPMreq(s uintptr, name int, ifi *net.Interface, grp net.IP) error {
 	mreq := sysIPMreq{Multiaddr: [4]byte{grp[0], grp[1], grp[2], grp[3]}}
 	if err := setIPMreqInterface(&mreq, ifi); err != nil {
 		return err
 	}
-	return os.NewSyscallError("setsockopt", syscall.Setsockopt(fd, iana.ProtocolIP, int32(name), (*byte)(unsafe.Pointer(&mreq)), int32(sysSizeofIPMreq)))
+	return os.NewSyscallError("setsockopt", setsockopt(s, iana.ProtocolIP, name, unsafe.Pointer(&mreq), sysSizeofIPMreq))
 }
 
-func getsockoptInterface(fd syscall.Handle, name int) (*net.Interface, error) {
+func getsockoptInterface(s uintptr, name int) (*net.Interface, error) {
 	var b [4]byte
-	l := int32(4)
-	if err := syscall.Getsockopt(fd, iana.ProtocolIP, int32(name), (*byte)(unsafe.Pointer(&b[0])), &l); err != nil {
+	l := uint32(4)
+	if err := getsockopt(s, iana.ProtocolIP, name, unsafe.Pointer(&b[0]), &l); err != nil {
 		return nil, os.NewSyscallError("getsockopt", err)
 	}
 	ifi, err := netIP4ToInterface(net.IPv4(b[0], b[1], b[2], b[3]))
@@ -34,12 +35,12 @@ func getsockoptInterface(fd syscall.Handle, name int) (*net.Interface, error) {
 	return ifi, nil
 }
 
-func setsockoptInterface(fd syscall.Handle, name int, ifi *net.Interface) error {
+func setsockoptInterface(s uintptr, name int, ifi *net.Interface) error {
 	ip, err := netInterfaceToIP4(ifi)
 	if err != nil {
 		return err
 	}
 	var b [4]byte
 	copy(b[:], ip)
-	return os.NewSyscallError("setsockopt", syscall.Setsockopt(fd, iana.ProtocolIP, int32(name), (*byte)(unsafe.Pointer(&b[0])), 4))
+	return os.NewSyscallError("setsockopt", setsockopt(s, iana.ProtocolIP, name, unsafe.Pointer(&b[0]), uint32(4)))
 }
