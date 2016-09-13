41376c70fdbb755dd0d77557c8612255a300ac8b
asaka <aisk1988@gmail.com>
2016-09-13 21:06:37 +0800

    feat: support leancache commands

---
+++ lean/api/leancache.go
@@ -0,0 +1,83 @@
+package api
+
+import "fmt"
+
+// GetCacheListResult is GetCacheList's return structure type
+type GetCacheListResult struct {
+	Instance   string `json:"instance"`
+	MaxMemory  int    `json:"max_memory"`
+	InstanceID string `json:"instance_id"`
+	Info       struct {
+		UsedMemoryHuman string `json:"used_memory_human"`
+	} `json:"info"`
+}
+
+// GetCacheList returns current app's LeanCache instance list
+func GetCacheList(appID string) ([]*GetCacheListResult, error) {
+	// TODO: we will use cookie based auth latter
+	appInfo, err := GetAppInfo(appID)
+	if err != nil {
+		return nil, err
+	}
+
+	region, err := GetAppRegion(appID)
+	if err != nil {
+		return nil, err
+	}
+	client := NewClient(region)
+
+	opts, err := client.options()
+	if err != nil {
+		return nil, err
+	}
+	opts.Headers["X-LC-Id"] = appID
+	opts.Headers["X-LC-Key"] = appInfo.MasterKey + ",master"
+
+	resp, err := client.get("/1.1/__cache/ops/instances", opts)
+	if err != nil {
+		return nil, err
+	}
+
+	var result []*GetCacheListResult
+	err = resp.JSON(&result)
+
+	return result, err
+}
+
+// ExecuteCacheCommandResult is ExecuteCacheCommand's result type
+type ExecuteCacheCommandResult struct {
+	Result interface{} `json:"result"`
+}
+
+// ExecuteCacheCommand will send command to LeanCache and excute it
+func ExecuteCacheCommand(appID string, instance string, db int, command string) (*ExecuteCacheCommandResult, error) {
+	// TODO: we will use cookie based auth latter
+	appInfo, err := GetAppInfo(appID)
+	if err != nil {
+		return nil, err
+	}
+
+	region, err := GetAppRegion(appID)
+	if err != nil {
+		return nil, err
+	}
+	client := NewClient(region)
+
+	opts, err := client.options()
+	if err != nil {
+		return nil, err
+	}
+	opts.Headers["X-LC-Id"] = appID
+	opts.Headers["X-LC-Key"] = appInfo.MasterKey + ",master"
+
+	url := fmt.Sprintf("/1.1/__cache/ops/instances/%s/dbs/%d", instance, db)
+
+	resp, err := client.post(url, map[string]interface{}{
+		"command": command,
+	}, opts)
+
+	result := new(ExecuteCacheCommandResult)
+	err = resp.JSON(result)
+
+	return result, err
+}
