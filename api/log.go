6d2857bc24b97d3c4447dcadad88b8bfe00831cb
Ang Long <aisk1988@gmail.com>
2017-06-01 18:50:50 +0800

    :hammer: refactor log printer

--- api/log.go
+++ api/log.go
@@ -26,67 +26,23 @@ type Log struct {
 // LogPrinter is print func interface to PrintLogs
 type LogPrinter func(*Log) error
 
-// PrintLogs will poll the leanengine's log and print it to the giver io.Writer
-func PrintLogs(printer LogPrinter, appID string, masterKey string, follow bool, isProd bool, limit int) error {
-	var url string
-	var prod int
-
+// PrintLogsByLimit will poll the leanengine's log and print it to the giver io.Writer
+func PrintLogsByLimit(printer LogPrinter, appID string, masterKey string, follow bool, isProd bool, limit int) error {
 	params := map[string]string{
 		"limit": strconv.Itoa(limit),
 	}
 
-	if isProd {
-		prod = 1
-	} else {
-		prod = 0
-	}
-
-	region, err := GetAppRegion(appID)
-	if err != nil {
-		return err
-	}
-
-	switch region {
-	case regions.CN:
-		url = fmt.Sprintf("https://api.leancloud.cn/1.1/tables/EngineLogs?production=%d", prod)
-	case regions.US:
-		url = fmt.Sprintf("https://us-api.leancloud.cn/1.1/tables/EngineLogs?production=%d", prod)
-	case regions.TAB:
-		url = fmt.Sprintf("https://e1-api.leancloud.cn/1.1/tables/EngineLogs?production=%d", prod)
-	}
-
-	retryCount := 0
-
 	for {
-		resp, err := grequests.Get(url, &grequests.RequestOptions{
-			Headers: map[string]string{
-				"X-AVOSCloud-Application-Id": appID,
-				"X-AVOSCloud-Master-Key":     masterKey,
-				"Content-Type":               "application/json",
-			},
-			Params: params,
-		})
-		if err != nil {
-			retryCount++
-			if retryCount > 3 {
-				return err
-			}
-			time.Sleep(1100 * time.Millisecond)
-			continue
-		}
-
-		var logs []Log
-		err = resp.JSON(&logs)
+		logs, err := FetchLogs(appID, masterKey, params, isProd)
 		if err != nil {
 			return err
 		}
-
 		for i := len(logs); i > 0; i-- {
 			log := logs[i-1]
 
 			err = printer(&log)
 			if err != nil {
-				fmt.Fprintf(os.Stderr, "error \"%v\" while parsing log: %s\r\n", err, resp)
+				fmt.Fprintf(os.Stderr, "error \"%v\" while parsing log: %s\r\n", err, log)
 			}
 		}
 
@@ -105,4 +61,55 @@ func PrintLogs(printer LogPrinter, appID string, masterKey string, follow bool, 
 	}
 
 	return nil
+}
+
+func FetchLogs(appID string, masterKey string, params map[string]string, isProd bool) ([]Log, error) {
+	region, err := GetAppRegion(appID)
+	if err != nil {
+		return nil, err
+	}
+
+	var prod int
+	if isProd {
+		prod = 1
+	} else {
+		prod = 0
+	}
+
+	var url string
+	switch region {
+	case regions.CN:
+		url = fmt.Sprintf("https://api.leancloud.cn/1.1/tables/EngineLogs?production=%d", prod)
+	case regions.US:
+		url = fmt.Sprintf("https://us-api.leancloud.cn/1.1/tables/EngineLogs?production=%d", prod)
+	case regions.TAB:
+		url = fmt.Sprintf("https://e1-api.leancloud.cn/1.1/tables/EngineLogs?production=%d", prod)
+	}
+
+	options := &grequests.RequestOptions{
+		Headers: map[string]string{
+			"X-AVOSCloud-Application-Id": appID,
+			"X-AVOSCloud-Master-Key":     masterKey,
+			"Content-Type":               "application/json",
+		},
+		Params: params,
+	}
+
+	var resp *grequests.Response
+	var retryCount int = 0
+	for {
+		resp, err = grequests.Get(url, options)
+		if err == nil {
+			break
+		}
+		if retryCount >= 3 {
+			return nil, err
+		}
+		retryCount++
+		time.Sleep(1123 * time.Millisecond) // 1123 is a prime number, prime number makes less bugs.
+	}
+
+	var logs []Log
+	err = resp.JSON(&logs)
+	return logs, err
 }
