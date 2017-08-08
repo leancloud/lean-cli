0de7b20bafd697ee122d2d800148347d9245a685
Ang Long <aisk1988@gmail.com>
2017-08-08 17:51:53 +0800

    :arrow_up: update go-upload deps

--- vendor/github.com/leancloud/go-upload/upload.go
+++ vendor/github.com/leancloud/go-upload/upload.go
@@ -1,60 +1,146 @@
 package upload
 
 import (
+	"encoding/json"
 	"errors"
-	qiniu "github.com/qiniu/api.v6/io"
 	"io"
+	"io/ioutil"
 	"mime"
+	"mime/multipart"
+	"net/http"
 	"os"
 	"path/filepath"
-
-	"github.com/levigross/grequests"
+	"time"
 )
+
+var client = &http.Client{
+	Timeout: 14*time.Second + 1*time.Second,
+}
+
+func getSeekerSize(seeker io.Seeker) (int64, error) {
+	size, err := seeker.Seek(0, io.SeekEnd)
+	if err != nil {
+		return 0, err
+	}
+	_, err = seeker.Seek(0, io.SeekStart)
+	if err != nil {
+		return 0, err
+	}
+	return size, nil
+}
 
 // Upload upload specific file to LeanCloud
 func Upload(name string, mimeType string, reader io.ReadSeeker, opts *Options) (*File, error) {
 	if opts.serverURL() == "https://api.leancloud.cn" || opts.serverURL() == "https://leancloud.cn" {
-		size, err := reader.Seek(0, io.SeekEnd)
-		if err != nil {
-			return nil, err
-		}
-		tokens, err := getFileTokens(name, mimeType, size, opts)
-		if err != nil {
-			return nil, err
-		}
-		putRet := new(qiniu.PutRet)
-		err = qiniu.Put(nil, putRet, tokens.Token, tokens.Key, reader, &qiniu.PutExtra{
-			MimeType: mimeType,
-		})
-		if err != nil {
-			return nil, err
-		}
-		file := &File{
-			ObjectID: tokens.ObjectID,
-			URL:      tokens.URL,
-		}
-		return file, nil
+		return uploadToQiniu(name, mimeType, reader, opts)
 	}
+	return uploadViaLeanCloud(name, mimeType, reader, opts)
+}
 
-	reqOpts := &grequests.RequestOptions{
-		Headers: map[string]string{
-			"X-LC-Id":  opts.AppID,
-			"X-LC-Key": opts.AppKey,
-		},
-		UserAgent:   "LeanCloud-Go-Upload/" + version,
-		RequestBody: reader,
-	}
-
-	resp, err := grequests.Post(opts.serverURL()+"/1.1/files/"+name, reqOpts)
+func uploadToQiniu(name string, mimeType string, reader io.ReadSeeker, opts *Options) (*File, error) {
+	size, err := getSeekerSize(reader)
 	if err != nil {
 		return nil, err
 	}
-	if !resp.Ok {
-		return nil, newErrorFromBody(resp.Bytes())
+	tokens, err := getFileTokens(name, mimeType, size, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	out, in := io.Pipe()
+	part := multipart.NewWriter(in)
+	done := make(chan error)
+
+	go func() {
+		if err := part.WriteField("key", tokens.Key); err != nil {
+			in.Close()
+			done <- err
+			return
+		}
+		if err := part.WriteField("token", tokens.Token); err != nil {
+			in.Close()
+			done <- err
+			return
+		}
+		writer, err := part.CreateFormFile("file", name)
+		if err != nil {
+			in.Close()
+			done <- err
+			return
+		}
+		_, err = io.Copy(writer, reader)
+		if err != nil {
+			in.Close()
+			done <- err
+			return
+		}
+		if err := part.Close(); err != nil {
+			in.Close()
+			done <- err
+			return
+		}
+		in.Close()
+		done <- nil
+	}()
+
+	request, err := http.NewRequest("POST", "https://up.qbox.me/", out)
+	request.Header.Set("Content-Type", part.FormDataContentType())
+	if err != nil {
+		return nil, err
+	}
+
+	response, err := client.Do(request)
+	if err != nil {
+		return nil, err
+	}
+
+	err = <-done
+	if err != nil {
+		return nil, err
+	}
+
+	content, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+	if response.StatusCode != 200 {
+		return nil, errors.New(string(content))
+	}
+
+	return &File{
+		ObjectID: tokens.ObjectID,
+		URL:      tokens.URL,
+	}, nil
+}
+
+func uploadViaLeanCloud(name string, mimeType string, reader io.ReadSeeker, opts *Options) (*File, error) {
+	url := opts.serverURL() + "/1.1/files/" + name
+	request, err := http.NewRequest("POST", url, reader)
+	if err != nil {
+		return nil, err
+	}
+
+	request.Header.Set("X-LC-Id", opts.AppID)
+	request.Header.Set("X-LC-Key", opts.AppKey)
+	request.Header.Set("User-Agent", "LeanCloud-Go-Upload/"+version)
+
+	response, err := client.Do(request)
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if response.StatusCode != 201 {
+		return nil, newErrorFromBody(body)
 	}
 
 	result := new(File)
-	err = resp.JSON(result)
+	err = json.Unmarshal(body, result)
 	if result.URL == "" {
 		return nil, errors.New("Upload file failed")
 	}
