abc38a7a2955aac3a41cb24059862cf1507dd291
Ang Long <aisk1988@gmail.com>
2017-08-18 18:27:20 +0800

    :white_check_mark: add login related test

---
+++ api/account_test.go
@@ -0,0 +1,42 @@
+package api
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/leancloud/lean-cli/api/regions"
+)
+
+func fakeHome(t *testing.T) func() {
+	originHome := os.Getenv("HOME")
+	tmpHome, err := ioutil.TempDir("", "lea-cli-test-home-")
+	if err != nil {
+		t.Fatal(err)
+	}
+	os.Setenv("HOME", tmpHome)
+	return func() {
+		os.Setenv("HOME", originHome)
+	}
+}
+
+func TestLoginCN(t *testing.T) {
+	defer fakeHome(t)()
+
+	_, err := Login("hife@amail.club", "A12345678", regions.CN)
+	if err != nil {
+		t.Error(err)
+	}
+	_, err = Login("hife@amail.club", "A12345678", regions.TAB)
+	if err != nil {
+		t.Error(err)
+	}
+
+	loginedRegions, err := GetLoginedRegion()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(loginedRegions) != 2 {
+		t.Error()
+	}
+}
