fe4667fed11ab8469f32477a632ed12545aec172
jysperm <jysperm@gmail.com>
2018-01-26 15:29:10 +0800

    :boom: No longer migrate from legency project config

--- apps/app.go
+++ apps/app.go
@@ -1,14 +1,11 @@
 package apps
 
 import (
-	"encoding/json"
 	"errors"
 	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
-
-	"github.com/aisk/logp"
 )
 
 var (
@@ -55,9 +52,6 @@ func LinkGroup(projectPath string, groupName string) error {
 // GetCurrentAppID will return the content of ${projectPath}/.leancloud/current_app_id
 func GetCurrentAppID(projectPath string) (string, error) {
 	content, err := ioutil.ReadFile(currentAppIDFilePath(projectPath))
-	if os.IsNotExist(err) {
-		return migrateLegencyProjectConfig(projectPath)
-	}
 	if err != nil {
 		return "", err
 	}
@@ -73,9 +67,6 @@ func GetCurrentAppID(projectPath string) (string, error) {
 // or migrate the project's primary group.
 func GetCurrentGroup(projectPath string) (string, error) {
 	content, err := ioutil.ReadFile(currentGroupFilePath(projectPath))
-	if os.IsNotExist(err) {
-		return migrateLegencyGroupProjectConfig(projectPath)
-	}
 	if err != nil {
 		return "", err
 	}
@@ -85,64 +76,4 @@ func GetCurrentGroup(projectPath string) (string, error) {
 		return "", errors.New(msg)
 	}
 	return groupName, nil
-}
-
-func getLegencyAppID(projectPath string) (string, error) {
-	content, err := ioutil.ReadFile(filepath.Join(projectPath, ".avoscloud", "apps.json"))
-	if err != nil {
-		return "", ErrNoAppLinked
-	}
-
-	var apps map[string]string
-	err = json.Unmarshal(content, &apps)
-	if err != nil {
-		return "", ErrNoAppLinked
-	}
-
-	if len(apps) == 0 {
-		return "", ErrNoAppLinked
-	}
-
-	if len(apps) == 1 {
-		for _, v := range apps {
-			return v, nil
-		}
-	}
-
-	content, err = ioutil.ReadFile(filepath.Join(projectPath, ".avoscloud", "curr_app"))
-	if err != nil {
-		return "", ErrNoAppLinked
-	}
-	appName := string(content)
-
-	appID, ok := apps[appName]
-	if !ok {
-		return "", ErrNoAppLinked
-	}
-	return appID, nil
-}
-
-func migrateLegencyProjectConfig(projectPath string) (string, error) {
-	appID, err := getLegencyAppID(projectPath)
-	if err != nil {
-		return "", err
-	}
-
-	logp.Warn("检测到旧版命令行工具项目配置，正在迁移 ...")
-	err = LinkApp(projectPath, appID)
-	if err != nil {
-		return "", err
-	}
-
-	logp.Warnf("迁移完毕，`%s`可进行删除\r\n", filepath.Join(projectPath, ".avoscloud"))
-
-	return appID, nil
-}
-
-func migrateLegencyGroupProjectConfig(projectPath string) (string, error) {
-	logp.Warn("检测到当前项目没有关联分组，迁移项目至默认分组(web)")
-	if err := ioutil.WriteFile(currentGroupFilePath(projectPath), []byte("web"), 0644); err != nil {
-		return "", err
-	}
-	return "web", nil
 }
