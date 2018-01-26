ac4f80f5c7a3c49538f56395b572629cfb2d36e4
jysperm <jysperm@gmail.com>
2018-01-26 15:40:50 +0800

    :card_file_box: Error when missing app region cache

--- api/app_router.go
+++ api/app_router.go
@@ -2,53 +2,21 @@ package api
 
 import (
 	"encoding/json"
-	"fmt"
+	"errors"
 	"io/ioutil"
 	"path/filepath"
 
 	"github.com/leancloud/lean-cli/api/regions"
 	"github.com/leancloud/lean-cli/utils"
-	"github.com/leancloud/lean-cli/version"
-	"github.com/levigross/grequests"
 )
 
 var routerCache = make(map[string]regions.Region)
 
-// GetAppRegion will query the app router, and return the app's region.
-// The result is cached in process memory.
 func GetAppRegion(appID string) (regions.Region, error) {
 	if r, ok := routerCache[appID]; ok {
 		return r, nil
-	}
-	resp, err := grequests.Get("https://app-router.leancloud.cn/1/route?appId="+appID, &grequests.RequestOptions{
-		UserAgent: "LeanCloud-CLI/" + version.Version,
-	})
-	if err != nil {
-		return regions.Invalid, err
-	}
-	if !resp.Ok {
-		return regions.Invalid, fmt.Errorf("query app router failed: %d", resp.StatusCode)
-	}
-
-	var result struct {
-		APIServer string `json:"api_server"`
-	}
-	if err = resp.JSON(&result); err != nil {
-		return regions.Invalid, err
-	}
-
-	switch result.APIServer {
-	case "us-api.leancloud.cn":
-		routerCache[appID] = regions.US
-		return regions.US, nil
-	case "api.leancloud.cn":
-		routerCache[appID] = regions.CN
-		return regions.CN, nil
-	case "e1-api.leancloud.cn":
-		routerCache[appID] = regions.TAB
-		return regions.TAB, nil
-	default:
-		return regions.Invalid, fmt.Errorf("invalid region server: %s", result.APIServer)
+	} else {
+		return regions.Invalid, errors.New("应用配置信息不完整，请重新运行 `lean switch` 关联应用")
 	}
 }
 
