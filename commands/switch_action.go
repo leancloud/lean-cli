de79501a758850ac1e24d6f0cd1130a9d3b73b76
jysperm <jysperm@gmail.com>
2019-01-17 16:47:52 +0800

    :bug: Fix `lean switch` panic when no apps available

--- commands/switch_action.go
+++ commands/switch_action.go
@@ -121,7 +121,7 @@ func checkOutWithWizard(regionString string, groupName string) error {
 	case "":
 		loginedRegions := apps.GetLoginedRegions()
 		if len(loginedRegions) == 0 {
-
+			return cli.NewExitError("No apps available in logged regions", 1)
 		} else if len(loginedRegions) == 1 {
 			region = loginedRegions[0]
 		} else {
