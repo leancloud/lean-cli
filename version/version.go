0ddd7144a1d4e23ee8d39f5099dbfc52d5824b55
jysperm <jysperm@gmail.com>
2021-05-10 16:05:15 +0800

    :bookmark: Release 0.24.4

--- version/version.go
+++ version/version.go
@@ -5,7 +5,7 @@ import (
 )
 
 // Version is lean-cli's version.
-const Version = "0.24.3"
+const Version = "0.24.4"
 
 func PrintCurrentVersion() {
 	logp.Info("Current CLI tool version: ", Version)
