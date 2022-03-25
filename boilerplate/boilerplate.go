f8906de8118a0c4f55f789c3a961f1cd5feaee95
jysperm <jysperm@gmail.com>
2022-03-25 15:24:38 +0800

    :sparkles: Create React or Vue project via lean new

--- boilerplate/boilerplate.go
+++ boilerplate/boilerplate.go
@@ -3,9 +3,11 @@ package boilerplate
 import (
 	"archive/zip"
 	"errors"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strconv"
 	"strings"
@@ -19,6 +21,158 @@ import (
 	"github.com/levigross/grequests"
 	"github.com/mattn/go-colorable"
 )
+
+func CreateProject(boil *Boilerplate, dest string, appID string, region regions.Region) error {
+	if boil.DownloadURL != "" {
+		if err := os.Mkdir(dest, 0775); err != nil {
+			return err
+		}
+
+		dir, err := ioutil.TempDir("", "leanengine")
+		if err != nil {
+			return err
+		}
+		defer os.RemoveAll(dir)
+		zipFilePath := filepath.Join(dir, "getting-started.zip")
+
+		var downloadURLs []string
+		if region.InChina() {
+			downloadURLs = []string{"https://releases.leanapp.cn", "https://api.github.com/repos"}
+		} else {
+			downloadURLs = []string{"https://api.github.com/repos", "https://releases.leanapp.cn"}
+		}
+		err = downloadToFile(downloadURLs[0]+boil.DownloadURL, zipFilePath)
+		if err != nil {
+			err = downloadToFile(downloadURLs[1]+boil.DownloadURL, zipFilePath)
+			if err != nil {
+				return err
+			}
+		}
+
+		logp.Info("Creating project...")
+
+		zipFile, err := zip.OpenReader(zipFilePath)
+		if err != nil {
+			return err
+		}
+		defer zipFile.Close()
+		for _, f := range zipFile.File {
+			// Remove outer directory name.
+			f.Name = f.Name[strings.Index(f.Name, "/"):]
+			err := extractAndWriteFile(f, dest)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	if boil.CMD != nil {
+		args := boil.CMD(dest)
+		cmd := exec.Command(args[0], args[1:]...)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+
+		stdin, err := cmd.StdinPipe()
+
+		if err != nil {
+			return err
+		}
+
+		go func() {
+			defer stdin.Close()
+			io.Copy(stdin, os.Stdin)
+		}()
+
+		if err := cmd.Run(); err != nil {
+			return err
+		}
+	}
+
+	if boil.Files != nil {
+		for name, body := range boil.Files {
+			if err := ioutil.WriteFile(filepath.Join(dest, name), []byte(body), 0644); err != nil {
+				return err
+			}
+		}
+	}
+
+	logp.Info(fmt.Printf("Created %s project in `%s`", boil.Name, dest))
+
+	if boil.Message != "" {
+		logp.Info(boil.Message)
+	}
+
+	return nil
+}
+
+type Boilerplate struct {
+	Name        string
+	Message     string
+	DownloadURL string
+	CMD         func(dest string) []string
+	Files       map[string]string
+}
+
+var Boilerplates = []Boilerplate{
+	{
+		Name:        "Node.js - Express",
+		Message:     "Lean how to use Express at https://expressjs.com",
+		DownloadURL: "/leancloud/node-js-getting-started/zipball/master",
+	},
+	{
+		Name:        "Node.js - Koa",
+		DownloadURL: "/leancloud/koa-getting-started/zipball/master",
+		Message:     "Lean how to use Koa at https://koajs.com",
+	},
+	{
+		Name:        "Python - Flask",
+		DownloadURL: "/leancloud/python-getting-started/zipball/master",
+		Message:     "Lean how to use Flask at https://flask.palletsprojects.com",
+	},
+	{
+		Name:        "Python - Django",
+		DownloadURL: "/leancloud/django-getting-started/zipball/master",
+		Message:     "Lean how to use Django at https://docs.djangoproject.com",
+	},
+	{
+		Name:        "Java - Serlvet",
+		DownloadURL: "/leancloud/servlet-getting-started/zipball/master",
+	},
+	{
+		Name:        "Java - Spring Boot",
+		DownloadURL: "/leancloud/spring-boot-getting-started/zipball/master",
+		Message:     "Lean how to use Spring Boot at https://spring.io/projects/spring-boot",
+	},
+	{
+		Name:        "PHP - Slim",
+		DownloadURL: "/leancloud/slim-getting-started/zipball/master",
+		Message:     "Lean how to use Slim at https://www.slimframework.com",
+	},
+	{
+		Name:        ".NET Core",
+		DownloadURL: "/leancloud/dotnet-core-getting-started/zipball/master",
+		Message:     "Lean how to use .NET Core at https://docs.microsoft.com/aspnet/core/",
+	},
+	{
+		Name:        "Go - Echo",
+		DownloadURL: "/leancloud/golang-getting-started/zipball/master",
+		Message:     "Lean how to use Echo at https://echo.labstack.com/",
+	},
+	{
+		Name:  "Web App - React (via create-react-app, require NPM installed)",
+		Files: prepareWebAppFiles("build"),
+		CMD: func(dest string) []string {
+			return []string{"npx", "create-react-app", dest, "--use-npm"}
+		},
+	},
+	{
+		Name:  "Web App - Vue (via @vue/cli, require NPM installed)",
+		Files: prepareWebAppFiles("dist"),
+		CMD: func(dest string) []string {
+			return []string{"npx", "@vue/cli", "create", "--default", "--packageManager", "npm", dest}
+		},
+	},
+}
 
 // don't know why archive/zip.Reader.File[0].FileInfo().IsDir() always return true,
 // this is a trick hack to void this.
@@ -55,61 +209,8 @@ func extractAndWriteFile(f *zip.File, dest string) error {
 	return nil
 }
 
-// FetchRepo will download the boilerplate from remote and extract to ${dest}/folder
-func FetchRepo(boil *Boilerplate, dest string, appID string, region regions.Region) error {
-	if err := os.Mkdir(dest, 0775); err != nil {
-		return err
-	}
-
-	dir, err := ioutil.TempDir("", "leanengine")
-	if err != nil {
-		return err
-	}
-	defer os.RemoveAll(dir)
-	zipFilePath := filepath.Join(dir, "getting-started.zip")
-
-	var downloadURLs []string
-	if region.InChina() {
-		downloadURLs = []string{"https://releases.leanapp.cn", "https://api.github.com/repos"}
-	} else {
-		downloadURLs = []string{"https://api.github.com/repos", "https://releases.leanapp.cn"}
-	}
-	err = DownloadToFile(downloadURLs[0]+boil.URL, zipFilePath)
-	if err != nil {
-		err = DownloadToFile(downloadURLs[1]+boil.URL, zipFilePath)
-		if err != nil {
-			return err
-		}
-	}
-
-	logp.Info("Creating project...")
-
-	zipFile, err := zip.OpenReader(zipFilePath)
-	if err != nil {
-		return err
-	}
-	defer zipFile.Close()
-	for _, f := range zipFile.File {
-		// Remove outer directory name.
-		f.Name = f.Name[strings.Index(f.Name, "/"):]
-		err := extractAndWriteFile(f, dest)
-		if err != nil {
-			return err
-		}
-	}
-	// TODO: Change value of boil.Homepage for English site.
-	logp.Info("Creating", boil.Name, "succeeded. Please refer to the website", boil.Homepage, "for documentation")
-	return nil
-}
-
-type Boilerplate struct {
-	Name     string `json:"name"`
-	Homepage string `json:"homepage"`
-	URL      string `json:"url"`
-}
-
-// DownloadToFile allows you to download the contents of the URL to a file
-func DownloadToFile(url string, fileName string) error {
+// downloadToFile allows you to download the contents of the URL to a file
+func downloadToFile(url string, fileName string) error {
 	resp, err := grequests.Get(url, &grequests.RequestOptions{
 		UserAgent: "LeanCloud-CLI/" + version.Version,
 	})
@@ -153,55 +254,14 @@ func DownloadToFile(url string, fileName string) error {
 	return nil
 }
 
-var Boilerplates = []Boilerplate{
-	{
-		Name:     "Express - Node.js",
-		URL:      "/leancloud/node-js-getting-started/zipball/master",
-		Homepage: "http://expressjs.com/",
-	},
-	{
-		Name:     "Koa - Node.js",
-		URL:      "/leancloud/koa-getting-started/zipball/master",
-		Homepage: "http://koajs.com/",
-	},
-	{
-		Name:     "Flask - Python",
-		URL:      "/leancloud/python-getting-started/zipball/master",
-		Homepage: "http://flask.pocoo.org/",
-	},
-	{
-		Name:     "Django - Python",
-		URL:      "/leancloud/django-getting-started/zipball/master",
-		Homepage: "https://www.djangoproject.com/",
-	},
-	{
-		Name:     "Serlvet - Java",
-		URL:      "/leancloud/java-war-getting-started/zipball/master",
-		Homepage: "https://jcp.org/en/jsr/detail?id=340",
-	},
-	{
-		Name:     "Spring Boot - Java",
-		URL:      "/leancloud/spring-boot-getting-started/zipball/master",
-		Homepage: "https://spring.io/projects/spring-boot",
-	},
-	{
-		Name:     "Slim - PHP",
-		URL:      "/leancloud/slim-getting-started/zipball/master",
-		Homepage: "http://www.slimframework.com/",
-	},
-	{
-		Name:     ".NET Core - .Net",
-		URL:      "/leancloud/dotnet-core-getting-started/zipball/master",
-		Homepage: "https://dotnet.microsoft.com/",
-	},
-	{
-		Name:     "Echo - Go",
-		URL:      "/leancloud/golang-getting-started/zipball/master",
-		Homepage: "https://echo.labstack.com/",
-	},
-	{
-		Name:     "Static Site",
-		URL:      "/leancloud/static-getting-started/zipball/master",
-		Homepage: "https://github.com/cloudhead/node-static",
-	},
+func prepareWebAppFiles(webRoot string) map[string]string {
+	return map[string]string{
+		"leanengine.yaml": "build: npm run build",
+		"static.json": fmt.Sprintf(`{
+  "public": "%s",
+  "rewrites": [
+    { "source": "**", "destination": "/index.html" }
+  ]
+}`, webRoot),
+	}
 }
